8a4742139851360c70577859338a1e8e0b137e8b
sacha <sacha@DESKTOP-K3ISJ8P>
2023-03-21 21:37:00 +0300

    Добавьте файлы проекта.

---
+++ Prakticheskaya_2_Zadanie_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Prakticheskaya_2_Zadanie_2
+{
+    class MyClass
+    {
+        private int n = 0;
+
+        public bool SetNumber(int number)
+        {
+            if (number == n + 1)
+            {
+                n = number;
+                return true;
+            }
+            else
+            {
+                n = 0;
+                return false;
+            }
+        }
+
+        public int GetExpectedNumber()
+        {
+            return n + 1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            while (true)
+            {
+                Console.WriteLine("Введите число " + myClass.GetExpectedNumber() + ":");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (myClass.SetNumber(number))
+                {
+                    Console.WriteLine("Правильно! Можете продолжить.");
+                }
+                else
+                {
+                    Console.WriteLine("Неправильно! Начните сначала.");
+                }
+            }
+        }
+    }
+}
